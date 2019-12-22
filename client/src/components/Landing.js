3ed3f83f3192b7e747867fb85be1a2e489ad18b2
Michael Giebner <mikegiebner@gmail.com>
2019-12-22 18:06:59 -0500

    Added Destination to the Landing page.

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -16,14 +16,18 @@ class Landing extends Component {
                   src="https://images.unsplash.com/photo-1571527036651-5860024b2351?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
                />
                <MainLandingImageButton
-                  label="Recipe"
-                  src="https://images.unsplash.com/photo-1551218808-94e220e084d2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                  label="Destination"
+                  src="https://images.unsplash.com/photo-1566760728319-20b86d2b419e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
                />
 
             </div>
 
             <div className="Landing-img-row">
 
+               <MainLandingImageButton
+                  label="Recipe"
+                  src="https://images.unsplash.com/photo-1551218808-94e220e084d2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+               />
                <MainLandingImageButton
                   label="Restaurant"
                   src="https://images.unsplash.com/photo-1489528792647-46ec39027556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
